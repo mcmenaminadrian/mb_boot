23e4c4f50b13aca6dba17d40c3ccf577ef34fdb0
Adrian McMenamin <adrianmcmenamin@gmail.com>
2014-11-10 23:01:26 +0000

    seems to run - in an endless loop

--- bootlink.s
+++ bootlink.s
@@ -1,6 +1,5 @@
 SECTIONS
 {
-	ENTRY(_start)
 	. = 0x0000;
 	.vectors.reset : { *(.vectors.reset) }
 	. = 0x0008;
@@ -8,8 +7,9 @@ SECTIONS
 	. = 0x0010;
 	.vectors.interrupt : { *(.vectors.interrupt) }
 	. = 0x0018;
+	.vectors.breakpoint : { *(.vectors.breakpoint) }
+	. = 0x0020;
 	.vectors.hw_exception : { *(.vectors.hw_exception) }
-	. = 0x100;
 	.text : { *(.text) }
 	.data : { *(.data) }
 	.bss : { *(.bss) }
